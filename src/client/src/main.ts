18862ee48df8b911a7351c62dc7e7d7153512ae0
Jan Merhar <merharjan@gmail.com>
2023-11-10 10:44:23 +0100

    Add Font Awesome Icons

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -2,8 +2,31 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/*
+ * Import Bootstrap
+ */
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
+/*
+ * Import Font Awesome Icons
+ */
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(fas, far, fab)
+
 const app = createApp(App)
 
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
