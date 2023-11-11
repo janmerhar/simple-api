2225eee7ff848daec0135517a87b8601847fd4f6
Jan Merhar <merharjan@gmail.com>
2023-11-11 19:10:25 +0100

    Add layout ReponsiveLayout.vue

---
+++ src/client/src/components/layout/ResponsiveLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container-fluid mt-5 pt-5">
+    <div class="row">
+      <div class="col-md-2 order-md-1"></div>
+      <div class="col order-md-2">
+        <slot />
+      </div>
+      <div class="col-md-2 order-md-3"></div>
+    </div>
+  </div>
+</template>
