7f88f25324bb347f8520d16af7b81362ccdf275f
Jan Merhar <merharjan@gmail.com>
2023-11-13 09:44:35 +0100

    Add CreateBreedAttributes.stub.ts

---
+++ src/client/src/composables/__tests__/stubs/CreateBreedAttributes.stub.ts
@@ -0,0 +1,10 @@
+export const CreateBreedAttributes = () => ({
+  name: 'Abyssinian',
+  country_code: 'EG',
+  weight_metric: '4 - 7',
+  life_span: '14 - 15',
+  description:
+    'The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.',
+  wikipedia_url: 'https://en.wikipedia.org/wiki/Abyssinian_(cat)',
+  reference_image_id: '0XYvRd7oD'
+})
