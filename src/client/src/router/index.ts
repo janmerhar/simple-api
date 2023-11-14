c58a75779918b9b269e6748fd6153bd916c9e86c
Jan Merhar <merharjan@gmail.com>
2023-11-14 19:18:23 +0100

    Add vue routes in index.ts

--- src/client/src/router/index.ts
+++ src/client/src/router/index.ts
@@ -5,10 +5,16 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/:page?',
       name: 'home',
       component: HomeView
-    }
+    },
+    {
+      path: '/gallery/:page?',
+      name: 'gallery',
+      component: () => import('../views/GalleryView.vue')
+    },
+    { path: '/:pathMatch(.*)*', name: '404', component: () => import('../views/NotFoundView.vue') }
   ]
 })
 
