4263532df5227c14bc3e39fa6a917f2eaca6d126
Jan Merhar <merharjan@gmail.com>
2023-11-14 08:29:35 +0100

    Add NotFoundView.vue

---
+++ src/client/src/views/NotFoundView.vue
@@ -0,0 +1,10 @@
+<template>
+  <p class="text-center fw-bold text-large">404</p>
+  <h3 class="text-uppercase text-center">Page Not Found</h3>
+</template>
+
+<style scoped>
+.text-large {
+  font-size: 14rem;
+}
+</style>
